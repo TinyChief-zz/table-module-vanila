f9d0cddeff9fb564eb580a39cc2e7636d5756aa6
TinyChief <narsacgm@gmail.com>
2018-11-15 17:05:14 +0300

    добавил подсветку, фильтрацию

--- src/index.js
+++ src/index.js
@@ -1,37 +1,68 @@
-import data from './MOCK_DATA.json';
+import users from './MOCK_DATA.json';
 
-const columnNames = [
-  'Name',
-  'Position',
-  'Office',
-  'Age',
-  'Start date',
-  'Salary',
-];
-
-// let tableItems = [];
+const columns = ['Name', 'Position', 'Office', 'Age', 'Start date', 'Salary'];
 
 class Table {
   currentPag = 0;
   tableItems = [];
-  init = () => {
+  columnArrowActive = null;
+  columnActive = null;
+  filteredData = null;
+  actualData = null;
+  columnNames = null;
+  pageLength = 10;
+  init = (data, columns, onPage) => {
+    // Если не даны имена колонок, то делаем именами ключю первого элемента data
+    columns
+      ? (this.columnNames = columns)
+      : (this.columnNames = Object.keys(data[0]));
+    // Устанавлием сколько строк на одной странице, по умолчанию 10
+    onPage ? (this.pageLength = onPage) : '';
+    // Хранит информацию в неизменном виде
+    this.actualData = data;
+    // Для реализации фильтрации, сортировки и т.д.
+    this.filteredData = data;
+    if (
+      document.querySelector('#search-button') &&
+      document.querySelector('#search-input')
+    ) {
+      document.querySelector('#search-button').addEventListener('click', e => {
+        e.preventDefault();
+        this.searchData();
+      });
+    }
+    // Инициализируем таблицу
     const tableW = document.querySelector('.table');
-    tableW.style.gridTemplateColumns = `repeat(${columnNames.length}, auto)`;
-    tableW.style.gridTemplateRows = `50px repeat(10, aut)`;
+    tableW.style.gridTemplateColumns = `repeat(${
+      this.columnNames.length
+    }, auto)`;
+    tableW.style.gridTemplateRows = `50px repeat(${this.pageLength}, aut)`;
     // COLUMN HEADER
-    columnNames.forEach(el => {
-      tableW.innerHTML += `<div class="column-title" key=${el} sort=1>${el}</div>`;
+    this.columnNames.forEach((el, i) => {
+      tableW.innerHTML += `
+      <div class="column-title" column=${i + 1} key=${el} sort=1>
+        ${el} 
+        <div class="arrows">
+          <span>&#9650;</span>
+          <span>&#9660;</span>
+        </div>
+      </div>`;
     });
     // TABLE ITEMS
-    for (let i = 0; i < columnNames.length * 10; i++) {
-      tableW.innerHTML += `<div class="table-item"></div>`;
+    for (let i = 0; i < this.columnNames.length * this.pageLength; i++) {
+      const columnNum = (i % this.columnNames.length) + 1;
+      if (Math.floor(i / this.columnNames.length) % 2 == 0) {
+        tableW.innerHTML += `<div column=${columnNum} class="table-item odd-row"></div>`;
+      } else {
+        tableW.innerHTML += `<div column=${columnNum} class="table-item even-row"></div>`;
+      }
     }
     this.tableItems = [...document.querySelectorAll('.table-item')];
     // PAGINATION
     const pagination = document.querySelector('.pagination');
     if (pagination) {
       pagination.innerHTML += `<li class="" key='prev'><span>Previous</span></li>`;
-      for (let i = 0; i < data.length / 10; i++) {
+      for (let i = 0; i < data.length / this.pageLength; i++) {
         pagination.innerHTML += `<li class="pagination-item" key=${i +
           1}><span>${i + 1}</span></li>`;
       }
@@ -56,9 +87,37 @@ class Table {
     const tableHeader = [...document.getElementsByClassName('column-title')];
     if (tableHeader) {
       tableHeader.forEach(el => {
+        // К каждому заглавию столбца добавляем обработчик клика по нему
         el.addEventListener('click', () => {
           let sortFlag = el.getAttribute('sort') == 1 ? 0 : 1;
           el.setAttribute('sort', sortFlag);
+          // Убираем подсветку стрелки у предыдущей, и добавляем подсветку на ту, на которую нажали
+          if (this.columnArrowActive) {
+            this.columnArrowActive.classList.remove('active');
+          } else {
+            this.columnArrowActive =
+              el.children[0].children[sortFlag == 1 ? 0 : 1];
+            this.columnActive = el.getAttribute('column');
+          }
+          this.columnArrowActive =
+            el.children[0].children[sortFlag == 1 ? 0 : 1];
+          el.children[0].children[sortFlag == 1 ? 0 : 1].classList.add(
+            'active',
+          );
+          // Убираем подсветку и добавляем подсветку отсортированного столбца
+          this.tableItems.forEach(item => {
+            if (
+              item.getAttribute('column') === this.columnActive &&
+              item.getAttribute('column') !== el.getAttribute('column')
+            ) {
+              item.classList.remove('sorted');
+            } else if (
+              item.getAttribute('column') === el.getAttribute('column')
+            ) {
+              item.classList.add('sorted');
+            }
+          });
+          this.columnActive = el.getAttribute('column');
           this.sortData(el.getAttribute('key').toLowerCase(), sortFlag);
         });
       });
@@ -66,27 +125,49 @@ class Table {
   };
   // Рендерит нужную информацию в ячейки таблицы. Таблица заполняется по змейке.
   render = startIndex => {
-    data.slice(startIndex, startIndex + 10).forEach((person, i) => {
-      Object.keys(person).forEach((el, j) => {
-        this.tableItems[i * columnNames.length + j].innerHTML = person[el];
+    if (this.filteredData.length < this.pageLength) {
+      this.clearTable();
+    }
+    this.filteredData
+      .slice(startIndex, startIndex + this.pageLength)
+      .forEach((person, i) => {
+        Object.keys(person).forEach((el, j) => {
+          this.tableItems[i * this.columnNames.length + j].innerHTML =
+            person[el];
+        });
       });
-    });
   };
   paginate = newPag => {
     this.currentPag = newPag;
-    this.render(newPag * columnNames.length);
+    this.render(newPag * this.columnNames.length);
   };
   sortData = (type, sortFlag) => {
     sortFlag == 1
-      ? data.sort((personA, personB) => personA[type] > personB[type])
-      : data.sort((personA, personB) => personA[type] < personB[type]);
+      ? this.filteredData.sort(
+          (personA, personB) => personA[type] > personB[type],
+        )
+      : this.filteredData.sort(
+          (personA, personB) => personA[type] < personB[type],
+        );
     this.currentPag = 0;
     this.render(0);
+  };
+  searchData = () => {
+    const searchString = document.querySelector('#search-input').value;
+    this.filteredData = this.actualData.filter(el => {
+      return el.name.includes(searchString);
+    });
+    this.render(0);
+  };
+  clearTable = () => {
+    for (let i = 0; i < this.columnNames.length * this.pageLength; i++) {
+      this.tableItems[i].innerHTML = '';
+    }
   };
 }
 
 const myTable = new Table();
 
-myTable.init();
+myTable.init(users, columns, 20);
 
 window.myTable = myTable;
